5e13fd902deacf9a2943cbd438c0c91ceb16ba02
FadilAzzam <fadilazzam0@gmail.com>
2024-11-04 19:40:17 +0700

    MuhamadFadilAzzam_0110124029_SI09

---
+++ soal2ddpke6.py
@@ -0,0 +1,7 @@
+# Buat lah output dari menggunakan bahasa pemprograman python dengan :
+# 1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = …
+hasil = 0
+for a in range(1, 20, 2):
+    hasil = hasil + a
+print(f"1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = {hasil}")
+    
